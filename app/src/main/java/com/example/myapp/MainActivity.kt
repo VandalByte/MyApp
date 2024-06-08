747ded563f0c7ef1a69f37af3ced0f5b1165a58d
Vandal <86826719+VandalByte@users.noreply.github.com>
2024-06-08 11:08:23 +0530

    Playing with Intents

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.example.myapp
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+    }
+
+    fun clickHandler(view: View) {
+        Log.i("button_click", "clickHandler: button clicked")
+        val dialIntent: Intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:9342345500"))
+        startActivity(dialIntent)
     }
 }
