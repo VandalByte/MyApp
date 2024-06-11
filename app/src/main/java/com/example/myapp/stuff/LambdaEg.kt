5eeeaad112b370085c4281e8cc8423fdb01639b8
Vandal <86826719+VandalByte@users.noreply.github.com>
2024-06-11 11:53:13 +0530

    did higer order function

--- app/src/main/java/com/example/myapp/stuff/LambdaEg.kt
+++ app/src/main/java/com/example/myapp/stuff/LambdaEg.kt
@@ -1,11 +1,13 @@
 package com.example.myapp.stuff
 
-fun main() {
-    val multiply: (Int, Int) -> Int = { a, b -> a * b }
-    println(multiply(3, 6))
+fun calculate(x: Int, y: Int, operation: (Int, Int) -> Int): Int {  // 1
+    return operation(x, y)                                          // 2
 }
 
-fun nullableFunction(): String? {
-    // returning null
-    return null
+fun sum(x: Int, y: Int) = x + y                                     // 3
+
+fun main() {
+    val sumResult = calculate(4, 5, ::sum)                          // 4
+    val mulResult = calculate(4, 5) { a, b -> a * b }               // 5
+    println("sumResult $sumResult, mulResult $mulResult")
 }
