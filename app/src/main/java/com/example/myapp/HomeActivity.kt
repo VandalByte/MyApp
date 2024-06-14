3ceb552d954cf58a00076d5f1b327f7a73e8b908
Vandal <86826719+VandalByte@users.noreply.github.com>
2024-06-14 11:36:53 +0530

    Playing with recycler view

--- app/src/main/java/com/example/myapp/HomeActivity.kt
+++ app/src/main/java/com/example/myapp/HomeActivity.kt
@@ -1,13 +1,31 @@
 package com.example.myapp
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    var dataArray = arrayOf("india","hindi","australia","peacock","blue")
+    lateinit var myRecycler: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.birthday_page)
+        setContentView(R.layout.row_layout_item)
+        myRecycler = findViewById(R.id.recyclerView)
+        myRecycler.layoutManager = LinearLayoutManager(this)
+        var wordsAdapter = WordsAdapter(dataArray)
+        myRecycler.adapter = wordsAdapter
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
